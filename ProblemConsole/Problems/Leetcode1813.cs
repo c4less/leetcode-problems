767ebc722dfa6de4eddd7d0402d5a9d24abb6469
Charles Raj <c_4less@live.in>
2024-10-13 19:20:48 -0400

    Commit Leetcode1813

---
+++ ProblemConsole/Problems/Leetcode1813.cs
@@ -0,0 +1,29 @@
+public class LeetCode1813 {
+    public static bool AreSentencesSimilar(string sentence1, string sentence2) {
+        
+        var sent1List = sentence1.Split(' ');
+        var sent2List = sentence2.Split(' ');
+
+        if(sent1List.Length < sent2List.Length)
+        {
+            (sent1List, sent2List) = (sent2List, sent1List);
+        }
+
+        int left =0;
+        
+        while(left < sent2List.Length
+        && sent1List[left] == sent2List[left]){
+            left++;
+        }
+
+        int right=0;
+        while(right < sent2List.Length &&
+        sent1List[sent1List.Length - 1-right]== sent2List[sent2List.Length - 1-right])
+        {
+            right++;
+        }
+
+
+        return left+right >= sent2List.Length;
+    }
+}
